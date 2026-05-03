c0d25521c4276ff22d8b2c18b5b4b9d005acfbc2
Sadhona Das <dassadhona684@gmail.com>
2026-05-03 23:36:09 +0600

    Input file

---
+++ lib/widgets/input_field.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class InputField extends StatelessWidget {
+  final TextEditingController controller;
+  final TextInputType keyboardType;
+  final String? Function(String?)? validator;
+  final String hintText;
+  final String labelText;
+  final Icon prefixIcon;
+
+  
+  const InputField({super.key,
+                    required this.controller,
+                   required this.keyboardType,
+                   required this.validator,
+                   required this.hintText,
+                   required this.labelText,
+                   required this.prefixIcon
+  });
+
+  @override
+Widget build(BuildContext context) {
+  return TextFormField(
+    controller: controller,
+    keyboardType: keyboardType,
+    obscureText: true,
+    validator: validator,
+    decoration: InputDecoration(
+      prefixIcon: prefixIcon,
+      hintText: hintText,
+      labelText: labelText,
+      
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(50),
+      ),
+    ),
+  );
+}
+}
